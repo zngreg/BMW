1863bc2055c94142b33238dc3cba22ed2e0a4d3b
Sanele Ngidi <sg.ngidi@live.co.za>
2025-09-27 08:44:16 +0200

    removed factory as I want both services to be exposed

--- BMW.Books.AuditingService/Program.cs
+++ BMW.Books.AuditingService/Program.cs
@@ -4,14 +4,10 @@ using BMW.Books.AuditingService.Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<UdpAuditListener>();
 builder.Services.AddSingleton<RabbitMqAuditListener>();
-builder.Services.AddSingleton<IAuditListenerFactory, AuditListenerFactory>();
 
 var app = builder.Build();
 
-var factory = app.Services.GetRequiredService<IAuditListenerFactory>();
 var cts = new CancellationTokenSource();
-// var listener = factory.CreateListener(app.Services);
-// _ = Task.Run(() => listener.StartAsync(cts.Token));
 
 var udpAuditListener = app.Services.GetRequiredService<UdpAuditListener>();
 _ = Task.Run(() => udpAuditListener.StartAsync(cts.Token));
